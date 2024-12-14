7e19687767da064465d064ae6097dae43e2f7f65
Aravintharajr <aravintharajr@gmail.com>
2024-12-14 11:32:45 +0000

    Effective modern C++ by Sott meyer. Type Deduction. How template function deduces. Example implemented.

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,56 +1,29 @@
 #include <iostream>
 #include <type_traits>
-#include <string>
 #include <typeinfo>
 
-// Template function to display type traits of a given argument
+// Template function to display type traits of both T and the Argument
 template <typename T>
-void displayTypeTraits(const T& arg) {
-    using ArgType = std::decay_t<decltype(arg)>;
-
-    std::cout << "Type traits for the given argument:" << std::endl;
-
-    // Determining the type using typeid
-    std::cout << "- Actual type: " << typeid(ArgType).name() << std::endl;
-
-    // Checking if the type is const
-    std::cout << "- Is const: " << std::is_const<ArgType>::value << std::endl;
-
-    // Checking if the type is volatile
-    std::cout << "- Is volatile: " << std::is_volatile<ArgType>::value << std::endl;
-
-    // Checking if the type is a reference
-    std::cout << "- Is reference: " << std::is_reference<decltype(arg)>::value << std::endl;
-
-    // Checking if the type is an lvalue reference
-    std::cout << "- Is lvalue reference: " << std::is_lvalue_reference<decltype(arg)>::value << std::endl;
-
-    // Checking if the type is an rvalue reference
-    std::cout << "- Is rvalue reference: " << std::is_rvalue_reference<decltype(arg)>::value << std::endl;
-
-    // Displaying the type as a string (basic type)
-    if(std::is_integral<ArgType>::value)
-        std::cout << "- Type: Integral type" << std::endl;
-    else if(std::is_floating_point<ArgType>::value)
-        std::cout << "- Type: Floating-point type" << std::endl;
-    else if(std::is_pointer<ArgType>::value)
-        std::cout << "- Type: Pointer type" << std::endl;
-    else
-        std::cout << "- Type: Unknown or complex type" << std::endl;
+void displayTypeTraits(const T& param) {
+    std::cout << "T:\t" << typeid(T).name() << " const:" << std::is_const<std::remove_reference_t<T>>::value << " refernce:" << std::is_reference<T>::value << std::endl;
+    std::cout << "param:\t" << typeid(param).name() <<  " const:" << std::is_const<std::remove_reference_t<decltype(param)>>::value << " refernce:" << std::is_reference<decltype(param)>::value << std::endl;
 }
 
 int main() {
-    int a = 10;
-    const int b = 20;
-    const int &c = a;
+    int x = 10;
+    const int cx = x;
+    const int& rx = x;
+
 
     // Function calls to display type traits
-    std::cout << "For variable 'int a = 10':\n";
-    displayTypeTraits(a);
-    std::cout << "\nFor variable 'const int b = 20':\n";
-    displayTypeTraits(b);
-    std::cout << "\nFor variable 'const int &c = a':\n";
-    displayTypeTraits(c);
+    std::cout << "For variable 'x':\n";
+    displayTypeTraits(x);
+    std::cout << "\nFor variable 'cx':\n";
+    displayTypeTraits(cx);
+    std::cout << "\nFor variable 'rx':\n";
+    displayTypeTraits(rx);
+    // std::cout << "\nFor rvalue 27:\n";
+    // displayTypeTraits(27);
 
     return 0;
 }
