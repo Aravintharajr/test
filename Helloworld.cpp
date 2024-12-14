3a85bd4a591ed45707a6e1b8608a8817d1dbb30d
Aravintharajr <aravintharajr@gmail.com>
2024-12-14 07:07:52 +0000

    Wanted to add a code to determind the constness and type of the function in the function. For the moment I am not sure how. Will try to find the deduced argument type and add the code when possible.

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <type_traits>
+
+template<typename T>
+void func(const T& param)
+{
+    if(std::is_const<decltype(param)>())
+        std::cout << "const ";
+    std::cout << typeid(param).name() << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello,World!" << std::endl;
+    int x = 10;
+    const int cx = x;
+    const int &rx = cx;
+    
+    func(x);
+    func(cx);
+    func(rx);
     return 0;
 }
